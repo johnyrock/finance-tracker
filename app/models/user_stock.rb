7ae04cd082ace6d857d3da94bda45ae5a5ac2aff
JDeveloper <johnreyroco@vista.aero>
2016-06-06 06:15:25 +0000

    Added validation for adding stocks ofr users

--- app/models/user_stock.rb
+++ app/models/user_stock.rb
@@ -2,6 +2,6 @@ class UserStock < ActiveRecord::Base
   belongs_to :user
   belongs_to :stock
   #validates_uniqueness_of :user_id, scope: :stock_id
-  validates :stock_id, :uniqueness => {:scope => :user_id, :message => ' was already added'}
+  #validates :stock_id, :uniqueness => {:scope => :user_id, :message => ' was already added'}
 
 end
