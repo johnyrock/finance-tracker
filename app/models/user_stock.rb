f3f19b3b5feafd6a78fa0af702f8f2b5c60db54b
JDeveloper <johnreyroco@vista.aero>
2016-06-06 03:41:26 +0000

    Added functionality to add stocks to a user and added validation fro duplicate userstock entries

--- app/models/user_stock.rb
+++ app/models/user_stock.rb
@@ -1,4 +1,7 @@
 class UserStock < ActiveRecord::Base
   belongs_to :user
   belongs_to :stock
+  #validates_uniqueness_of :user_id, scope: :stock_id
+  validates :stock_id, :uniqueness => {:scope => :user_id, :message => ' was already added'}
+
 end
