5b8fc4fffae3f58c885bd239fef42ebe24eaac94
JDeveloper <johnreyroco@vista.aero>
2016-06-22 09:15:48 +0000

    Added remove friend functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
     @friend = User.find(params[:friend])
     current_user.friendships.build(friend_id: @friend.id)
     
-    if current.save
+    if current_user.save
       redirect_to my_friends_path, notice: "Friend was successfully added"
     else
       redirect_to my_friends_path, flash[:error] = "There was an error with adding user as friend"
